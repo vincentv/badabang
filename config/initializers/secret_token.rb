21a9d0152642b8a11468be354d15c9b1d9bc764f
Vincentv <lsuntzu@gmail.com>
2011-10-15 03:27:31 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeadOrAlive::Application.config.secret_token = '911ca7ded16086bb54a1cb5f769fb32693a15cbedb1003c921c74e41811ee464940d4ed1e79d64104fc5e1fb33052160c30c12bc813a5fa191fcc40071b6dec7'
